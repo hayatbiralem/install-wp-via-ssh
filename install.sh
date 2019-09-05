ec4e8164060224732644db77c2ab9c5f34cb197b
Ömür Yanıkoğlu <hayatbiralem@gmail.com>
2019-09-05 11:17:11 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
-echo "Hello, world!"
-read -p "What is your name? " name
-echo "Hello, ${name}!"
+echo "Here we go!"
+read -p "Directory name? " blog_dir
+mkdir blog_dir
+cd blog_dir
+# wp core download
+read -p "DB Host?" dbhost
+read -p "DB Name?" dbname
+read -p "DB User?" dbuser
+echo -n "DB Password?"
+read -s dbpass
+read -p "Domain?" url
+read -p "Title?" title
+read -p "Admin Username?" admin_name
+echo -n "Admin Password?"
+read -s admin_password
+read -p "Admin Username?" admin_email
+# wp core config --dbhost=$dbhost --dbname=$dbname --dbuser=$dbuser --dbpass=$dbpass
+echo "--dbhost=$dbhost --dbname=$dbname --dbuser=$dbuser --dbpass=$dbpass"
+# chmod 644 wp-config.php
+# wp core install --url=$url --title=$title --admin_name=$admin_name --admin_password=$admin_password --admin_email=$admin_email
+echo "--url=$url --title=$title --admin_name=$admin_name --admin_password=$admin_password --admin_email=$admin_email"
